a53d91f2eb832916ea2e9001b96b89cb51b39481
elyzaandaya <elyzaandaya750@gmail.com>
2025-09-29 12:35:33 +0800

    update

--- app/config/database.php
+++ app/config/database.php
@@ -63,7 +63,7 @@ $database['main'] = array(
     'port'		=> '3306',
     'username'	=> 'sql12800495',
     'password'	=> '3RB4A1X8VQ',
-    'database'	=> 'sql12800495',
+    'database'	=> 'New',
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
